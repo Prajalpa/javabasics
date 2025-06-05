a05d23480eb47405ea8d51407799440baa5c3725
Prajalpa <prajalpatil2222@gmail.com>
2025-06-05 12:01:46 +0530

    hello

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+public static void main(String[] args){
+Boolean Iot=true;
+Boolean AI = false;
+System.out.println("I have Taken this subject" + Iot + "Not Taken this subject" + AI);
+int range=127;
+System.out.println(range);
+short range1=-120;
+
+System.out.println(range1);
+
+
+}
+}
